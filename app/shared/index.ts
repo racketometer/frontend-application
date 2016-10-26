4c207a1681040e1e18c5e0e04c3215d8ed1aa7f1
Bjørn <bjoern.soerensen@gmail.com>
2016-10-26 13:18:21 +0200

    Rename session service folder

--- app/shared/index.ts
+++ app/shared/index.ts
@@ -1,6 +1,6 @@
 export * from "./bluetooth.service";
 export * from "./overview.service";
-export * from "./session-service";
+export * from "./session";
 export * from "./util";
 export * from "./login";
 export * from "./user";
